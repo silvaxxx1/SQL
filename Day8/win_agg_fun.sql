d486e97f81ac69d9526a0938eccc4880bc84c370
silvaxxx1 <silvapi1994@gmail.com>
2025-06-27 20:41:00 +0300

    new Day8

---
+++ Day8/win_agg_fun.sql
@@ -0,0 +1,54 @@
+USE SalesDB 
+
+
+-- find the total number of sales (oveall analysis)
+-- provide details like order id and order date (high level aggregation)
+-- find the total number of orders for each customer 
+
+SELECT 
+    OrderID,
+    OrderDate,
+    CustomerID,
+    COUNT(*) OVER () TotalOrders,
+    COUNT(*) OVER (PARTITION BY CustomerID) OrderByCustomer
+ FROM Sales.Orders 
+
+
+-- FIND THE TOTAL NUMBER OF CUSTOMERS AND PROVIDE ALL CUSTOMER DETAILS 
+-- FIND THE TOTOAL NUMBER OF SCORES OF THE CUSTOMERS
+
+SELECT
+    * ,
+    COUNT(*) OVER () TotalCustomers,
+    COUNT(Score) OVER () TotalScores
+
+FROM Sales.Customers 
+
+
+-- FIND THE DUPLOCATE IN THE ORDERS TABLE 
+
+SELECT 
+    OrderID,
+    COUNT(*) OVER (PARTITION BY OrderID) CheckPK 
+ FROM Sales.Orders 
+
+SELECT 
+* 
+FROM (
+    SELECT
+        OrderID,
+        COUNT(*) OVER (PARTITION BY OrderID) CheckPK 
+    FROM Sales.OrdersArchive 
+
+)t WHERE CheckPK > 1  
+
+
+-- FIND THE PRECENTAGE CONTRIBUTION OF EACH PRODUCT TO THE TOTAL SALES 
+-- part to whole analysis 
+SELECT 
+    ProductID,
+    OrderID,
+    Sales,
+    SUM(Sales) OVER () TotalSales,
+    ROUND(CAST(Sales AS FLoat)/ SUM(Sales) OVER () * 100, 2)  PercentageContribution 
+FROM Sales.Orders
